8320eed9ed8d0175d72ae162c7f96a732922c631
dlcdlc5055 <72411751+dlcdlc5055@users.noreply.github.com>
2022-11-20 00:31:45 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar({DarkMode,CurrentPage,setCurrentPage}) {
     <div style={{color:LocalColor.textColor,backgroundColor:LocalColor.navBackgroundcolor,borderBottom:`2px solid ${LocalColor.borderColor}`}} className="navbarContainer">
       <div className='navbarContainerInner'>
         {buttons.map((it,i)=><button onMouseUp={()=>{setHoversDesktop(hover(i,true,HoversDesktop))}} onMouseDown={()=>{setHoversDesktop(hover(i,false,HoversDesktop))}} onMouseLeave={()=>{setHoversDesktop(hover(i,false,HoversDesktop))}} onMouseEnter={()=>{setHoversDesktop(hover(i,true,HoversDesktop))}}  key={i} onClick={()=>{setCurrentPage(i);setOpen(false)}} style={{backgroundColor:!HoversDesktop[i]?"#0000":LocalColor.backgroundActive,userSelect:"none",color:LocalColor.textColor}} className="NavLinkDesktop">{it}</button>)}
-        <button style={{backgroundColor:!HoverOpenBTN?"#0000":LocalColor.backgroundActive,userSelect:"none"}} onMouseUp={()=>{setHoverOpenBTN(true)}} onMouseDown={()=>{setHoverOpenBTN(false)}} onMouseLeave={()=>{setHoverOpenBTN(false)}} onMouseEnter={()=>{setHoverOpenBTN(true)}} onClick={()=>{setOpen(!Open)}} className={`${DarkMode?"mobileMenuButtonDark":"mobileMenuButtonLight"}`}>
+        <button style={{backgroundColor:!HoverOpenBTN?"#0000":LocalColor.backgroundActive,userSelect:"none"}} onMouseUp={()=>{setHoverOpenBTN(true)}} onMouseDown={()=>{setHoverOpenBTN(false)}} onMouseLeave={()=>{setHoverOpenBTN(false)}} onMouseEnter={()=>{setHoverOpenBTN(true)}} onClick={()=>{setOpen(!Open)}} className="mobileMenuButton">
           <div className={`layout_nav_mobile_btn ${Open?"layout_nav_mobile_btn_open":""}`}>
             <div style={{backgroundColor:LocalColor.borderColor}} className={`line ${Open?"nav-first-line":""}`}></div>
             <div style={{backgroundColor:LocalColor.borderColor}} className={`line ${Open?"nav-hide":""}`}></div>
@@ -28,7 +28,7 @@ export default function Navbar({DarkMode,CurrentPage,setCurrentPage}) {
         </button>
       </div>
       <div onClick={()=>{setOpen(false)}} className={`mobile-cover ${Open?"mobile-cover-open":""}`}></div>
-      <div className={`menuMobileContainer ${DarkMode?"menuMobileContainerDark":"menuMobileContainerLight"} ${Open?"menuMobileContainerOpen":""}`}>
+      <div style={{backgroundColor:LocalColor.navBackgroundcolor,borderLeftColor:LocalColor.borderColor}} className={`menuMobileContainer ${Open?"menuMobileContainerOpen":""}`}>
         {buttons.map((it,i)=><button onMouseUp={()=>{setHoversDesktop(hover(i,true,HoversDesktop))}} onMouseDown={()=>{setHoversDesktop(hover(i,false,HoversDesktop))}} onMouseLeave={()=>{setHoversMobile(hover(i,false,HoversDesktop))}} onMouseEnter={()=>{setHoversMobile(hover(i,true,HoversDesktop))}} key={i} style={{backgroundColor:!HoversMobile[i]?"#0000":LocalColor.backgroundActive,userSelect:"none",color:LocalColor.textColor}} onClick={()=>{setCurrentPage(i);setOpen(false)}} className="NavLinkMobile">{it}</button>)}
       </div>
     </div>
